7a80d45dc140fbd0444f67bc0b421ff441153f76
jalet <joakim@jarsater.com>
2022-12-02 22:28:19 +0100

    fmt

--- day01_1/src/bin/aoc2022rs_d01_1.rs
+++ day01_1/src/bin/aoc2022rs_d01_1.rs
@@ -2,10 +2,7 @@ fn main() {
     let res = include_str!("./input.txt")
         .trim()
         .split("\n\n")
-        .map(|x| {
-            return x.split("\n")
-             .map(|x| x.parse::<usize>().unwrap())
-        })
+        .map(|x| return x.split("\n").map(|x| x.parse::<usize>().unwrap()))
         .map(|x| x.sum::<usize>())
         .max();
 
--- day01_2/src/bin/aoc2022rs_d01_2.rs
+++ day01_2/src/bin/aoc2022rs_d01_2.rs
@@ -2,10 +2,7 @@ fn main() {
     let mut res = include_str!("./input.txt")
         .trim()
         .split("\n\n")
-        .map(|x| {
-            return x.split("\n")
-             .map(|x| x.parse::<usize>().unwrap())
-        })
+        .map(|x| return x.split("\n").map(|x| x.parse::<usize>().unwrap()))
         .map(|x| x.sum::<usize>())
         .collect::<Vec<_>>();
 
@@ -13,4 +10,3 @@ fn main() {
 
     print!("{:?}", res.iter().take(3).sum::<usize>());
 }
-
